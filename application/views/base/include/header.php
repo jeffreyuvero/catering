a493255cdff2fba118aefb3d856ca11d30d0ae28
Jeffrey <jeffreyequizuvero@gmail.com>
2018-08-19 15:09:58 +0800

    removed other design no have function

--- application/views/base/include/header.php
+++ application/views/base/include/header.php
@@ -62,7 +62,7 @@
       </button>
 
       <!-- Navbar Search -->
-      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+     <!--  <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
         <div class="input-group">
           <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
           <div class="input-group-append">
@@ -72,9 +72,9 @@
           </div>
         </div>
       </form>
-
+ -->
       <!-- Navbar -->
-      <ul class="navbar-nav ml-auto ml-md-0">
+      <!-- <ul class="navbar-nav ml-auto ml-md-0">
         <li class="nav-item dropdown no-arrow mx-1">
           <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-bell fa-fw"></i>
@@ -110,6 +110,6 @@
             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
           </div>
         </li>
-      </ul>
+      </ul> -->
 
     </nav>
